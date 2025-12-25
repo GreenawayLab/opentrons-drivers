cab99b4181560449f507285b811432eecff5d70b
alex.ost <adm89045@gmail.com>
2025-12-25 15:09:48 +0000

    make with work with external frame

--- opentrons_drivers/agent/agent_main.py
+++ opentrons_drivers/agent/agent_main.py
@@ -10,13 +10,10 @@ metadata = {
     "apiLevel": "2.13",
 }
 
-with open(Path('postbox', 'task.json')) as f:
-    task = json.load(f)
-    to_do = Path('protocols', task['who'], task['protocol'])
-    with open(Path(to_do, 'base_config.json')) as bc_file:
-        base_config = json.load(bc_file)
-    with open(Path(to_do, 'agent_config.json')) as ac_file:
-        agent_config = json.load(ac_file)
+with open(Path('postbox', 'base_config.json')) as bc_file:
+    base_config = json.load(bc_file)
+with open(Path('postbox', 'agent_config.json')) as ac_file:
+    agent_config = json.load(ac_file)
 
 def run(protocol: protocol_api.ProtocolContext) -> None:
     """Function triggered by the systemd unit opentrons_execute"""
